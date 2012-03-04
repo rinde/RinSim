32392a653550bc10b48655e797f79af59822ebcf
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-03-04 18:19:14 +0100

    added the Mercator projection and magic constant, now for REAL!

--- core/src/main/java/rinde/sim/util/OSM.java
+++ core/src/main/java/rinde/sim/util/OSM.java
@@ -35,9 +35,10 @@ public class OSM {
 		try {
 			InputSource inputSource = new InputSource(new FileInputStream(filename));
 			XMLReader xmlReader = XMLReaderFactory.createXMLReader();
-			//Multimap<Point, Point> graph = HashMultimap.create();
+			// Multimap<Point, Point> graph = HashMultimap.create();
 
-			TableGraph<MultiAttributeEdgeData> graph = new TableGraph<MultiAttributeEdgeData>(MultiAttributeEdgeData.EMPTY);
+			TableGraph<MultiAttributeEdgeData> graph = new TableGraph<MultiAttributeEdgeData>(
+					MultiAttributeEdgeData.EMPTY);
 
 			OSMParser parser = new OSMParser(graph);
 			xmlReader.setContentHandler(parser);
@@ -54,7 +55,7 @@ public class OSM {
 			for (Connection<MultiAttributeEdgeData> connection : removeList) {
 				graph.removeConnection(connection.from, connection.to);
 			}
-			System.out.println(highwayNames.toString());
+			// System.out.println(highwayNames.toString());
 			return graph;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -75,11 +76,12 @@ public class OSM {
 			nodes = new HashMap<String, Point>();
 		}
 
-		private static final long earthRadius = 63674490L;
+		// the earth radius in meters
+		private static final long earthRadius = 6378137L;
 
-		private double gradeToRadian(double grade) {
-			return grade * Math.PI / 180;
-		}
+		// private double gradeToRadian(double grade) {
+		// return grade * Math.PI / 180;
+		// }
 
 		@Override
 		public void startElement(String namespaceURI, String localName, String qualifiedName, Attributes attributes) {
@@ -88,19 +90,84 @@ public class OSM {
 			} else if (localName.equals("node")) {
 				double lat = Double.parseDouble(attributes.getValue("lat"));
 				double lon = Double.parseDouble(attributes.getValue("lon"));
-				//				LatLng latlong = new LatLng(lat, lon);
-				//latlong.toWGS84();
-				//				UTMRef ref = latlong.toUTMRef();
+				// LatLng latlong = new LatLng(lat, lon);
+				// latlong.toWGS84();
+				// UTMRef ref = latlong.toUTMRef();
 
-				lat = gradeToRadian(lat);
-				lon = gradeToRadian(lon);
-				long y = Math.round(earthRadius * Math.cos(lat) * Math.cos(lon));
-				long x = Math.round(earthRadius * Math.cos(lat) * Math.sin(lon));
+				// lat = gradeToRadian(lat);
+				// lon = gradeToRadian(lon);
+				// long y = Math.round(earthRadius * Math.cos(lat) *
+				// Math.cos(lon));
 
-				//ref.
+				// cos(latitude * pi/180) * earth circumference
 
-				//String[] converted = CoordinateConversion.latLon2MGRUTM(x, y).split(" ");
-				//				nodes.put(attributes.getValue("id"), new Point(Double.parseDouble(converted[2]), -Double.parseDouble(converted[3])));
+				// ground resolution = cos(latitude * pi/180) * earth
+				// circumference / map width
+				// = (cos(latitude * pi/180) * 2 * pi * 6378137 meters) / (256 *
+				// 2^level pixels)
+
+				// MAGIC constant! Don't touch this without consulting either
+				// Rinde van Lon or Bartosz Michalik, preferably both :-)
+				double scale = 1000000 / 1.425139046;// Math.toDegrees(Math.cos(Math.toRadians(lat)
+														// * Math.PI /
+														// Math.toRadians(180)))
+														// * 2 * Math.PI *
+														// earthRadius / 1000;
+
+				//
+
+				// sinLatitude = sin(latitude * pi/180)
+				//
+				// pixelX = ((longitude + 180) / 360) * 256 * 2 level
+				// pixelY = (0.5 - log((1 + sinLatitude) / (1 - sinLatitude)) /
+				// (4 * pi)) * 256 * 2 level
+
+				// double sinLattitude = Math.sin(Math.toRadians(lat * Math.PI /
+				// 180));
+				// double x = ((lon + 180) / 360.0);
+				// double y = (0.5 - Math.log((1 + sinLattitude) / (1 -
+				// sinLattitude)) / (4 * Math.PI));
+
+				// MERCATOR:
+				double x = scale * lon; // Math.round(earthRadius *
+										// Math.cos(lat) * Math.sin(lon));
+				double y = scale * Math.toDegrees(1.0 / Math.sinh(Math.tan(Math.toRadians(lat))));// Math.log(Math.tan(.25
+																									// *
+																									// Math.PI
+																									// +
+																									// .5
+																									// *
+																									// gradeToRadian(lat)));
+
+				// check: http://www.movable-type.co.uk/scripts/latlong.html for
+				// a great explanation
+
+				// converting to the Mercator projection:
+				// http://mathworld.wolfram.com/MercatorProjection.html
+				// Fun fact: Did you know that the Mercator projection was named
+				// after Gerardus Mercator (1512-1594), a Flemish cartographer?
+
+				// double dLat = (l)
+
+				// var R = 6371; // km
+				// var dLat = (lat2-lat1).toRad();
+				// var dLon = (lon2-lon1).toRad();
+				// var lat1 = lat1.toRad();
+				// var lat2 = lat2.toRad();
+				//
+				// var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+				// Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) *
+				// Math.cos(lat2);
+				// var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+				// var d = R * c;
+
+				// ref.
+
+				// String[] converted = CoordinateConversion.latLon2MGRUTM(x,
+				// y).split(" ");
+				// nodes.put(attributes.getValue("id"), new
+				// Point(Double.parseDouble(converted[2]),
+				// -Double.parseDouble(converted[3])));
 
 				nodes.put(attributes.getValue("id"), new Point(x, y));
 
@@ -118,8 +185,7 @@ public class OSM {
 		}
 
 		@Override
-		public void endDocument() {
-		}
+		public void endDocument() {}
 
 	}
 
@@ -155,15 +221,18 @@ public class OSM {
 
 				if (attributes.getValue("k").equals("oneway") && attributes.getValue("v").equals("yes")) {
 					oneWay = true;
-				} else if (attributes.getValue("k").equals("highway") && highwayTypes.contains(attributes.getValue("v"))) {
+				} else if (attributes.getValue("k").equals("highway")
+						&& highwayTypes.contains(attributes.getValue("v"))) {
 					isValidRoad = true;
-				} else if (attributes.getValue("k").equals("junction") && junctionTypes.contains(attributes.getValue("v"))) {
+				} else if (attributes.getValue("k").equals("junction")
+						&& junctionTypes.contains(attributes.getValue("v"))) {
 					isValidRoad = true;
 				} else if (attributes.getValue("k").equals("maxspeed")) {
 					try {
 						maxSpeed = Integer.parseInt(attributes.getValue("v").replaceAll("\\D", ""));
 					} catch (NumberFormatException nfe) {
-						// ignore if this happens, it means that no max speed was defined
+						// ignore if this happens, it means that no max speed
+						// was defined
 					}
 				}
 			} else if (localName.equals("nd")) {
@@ -177,6 +246,7 @@ public class OSM {
 					Point from = nodeMapping.get(nodes.get(i - 1));
 					Point to = nodeMapping.get(nodes.get(i));
 					if (from != null && to != null && !from.equals(to)) {
+
 						double length = Point.distance(from, to);
 						if (!graph.hasConnection(from, to)) {
 							graph.addConnection(from, to, new MultiAttributeEdgeData(length, maxSpeed));
