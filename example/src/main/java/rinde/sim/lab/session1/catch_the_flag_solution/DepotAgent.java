8c3a7806c5b31b178d4502aea473f9e712b48bd0
Robrecht Haesevoets <robrechth@gmail.com>
2012-03-09 15:29:59 +0100

    Added simple catch the flag solution

---
+++ example/src/main/java/rinde/sim/lab/session1/catch_the_flag_solution/DepotAgent.java
@@ -0,0 +1,115 @@
+package rinde.sim.lab.session1.catch_the_flag_solution;
+
+import java.util.Queue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import rinde.sim.core.Simulator;
+import rinde.sim.core.SimulatorAPI;
+import rinde.sim.core.SimulatorUser;
+import rinde.sim.core.TickListener;
+import rinde.sim.core.graph.Point;
+import rinde.sim.core.model.RoadModel;
+import rinde.sim.core.model.RoadUser;
+import rinde.sim.core.model.communication.CommunicationAPI;
+import rinde.sim.core.model.communication.CommunicationUser;
+import rinde.sim.core.model.communication.Mailbox;
+import rinde.sim.core.model.communication.Message;
+import rinde.sim.lab.common.ConfirmationMessage;
+import rinde.sim.lab.common.Flag;
+import rinde.sim.lab.common.TransportRequest;
+
+public class DepotAgent implements TickListener, RoadUser, SimulatorUser, CommunicationUser {
+	protected static final Logger LOGGER = LoggerFactory.getLogger(Simulator.class); 
+	
+	private Point startingLocation;
+
+	private RoadModel rm;
+	private CommunicationAPI cm;
+	private SimulatorAPI simulator;
+	
+	private Mailbox mailbox;
+	private Flag flag;
+	private long flagID = 0;
+	
+	public DepotAgent(Point startingLocation){
+		LOGGER.info("DepotAgent created.");
+		this.startingLocation = startingLocation;
+		this.mailbox = new Mailbox();
+	}
+	
+	@Override
+	public void initRoadUser(RoadModel model) {
+		this.rm = model;
+		this.rm.addObjectAt(this, startingLocation);
+	}
+
+	@Override
+	public void tick(long currentTime, long timeStep) {
+		//if there is no flag create a flag
+		if(flag == null){
+			placeFlag();
+		}
+	
+		//get all received messages (this empties the mailbox)
+		Queue<Message> messages = mailbox.getMessages();
+		for(Message message: messages){
+			if(message instanceof ConfirmationMessage){
+				ConfirmationMessage m = (ConfirmationMessage) message;
+				//check if confirmation is for current flag
+				if(m.getFlagID() == this.flagID){
+					simulator.unregister(flag);
+					placeFlag();
+					LOGGER.info("Depot: "+ m.getTruck() + " got the flag first");
+				}
+			}
+		}
+	}
+	
+	private void placeFlag(){
+		Point flagLocation = rm.getGraph().getRandomNode(simulator.getRandomGenerator());
+		flag = new Flag(flagLocation);
+		simulator.register(flag);
+		flagID ++;
+		cm.broadcast(new TransportRequest(this, flagLocation, flagID));
+	}
+
+	@Override
+	public void afterTick(long currentTime, long timeStep) {
+		//not used
+	}
+
+	@Override
+	public void setCommunicationAPI(CommunicationAPI api) {
+		this.cm = api;
+	}
+
+	@Override
+	public Point getPosition() {
+		return this.rm.getPosition(this);
+	}
+
+	@Override
+	public double getRadius() {
+		//distances are ignored in the communication model
+		return 0;
+	}
+
+	@Override
+	public double getReliability() {
+		//we do not consider reliability in this example
+		return 1;
+	}
+
+	@Override
+	public void receive(Message message) {
+		this.mailbox.receive(message);
+	}
+
+	@Override
+	public void setSimulator(SimulatorAPI api) {
+		this.simulator = api;
+	}
+
+}
---
+++ example/src/main/java/rinde/sim/lab/session1/catch_the_flag_solution/TruckAgent.java
@@ -0,0 +1,116 @@
+package rinde.sim.lab.session1.catch_the_flag_solution;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.apache.commons.math.random.RandomGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import rinde.sim.core.Simulator;
+import rinde.sim.core.SimulatorAPI;
+import rinde.sim.core.SimulatorUser;
+import rinde.sim.core.TickListener;
+import rinde.sim.core.graph.Point;
+import rinde.sim.core.model.MovingRoadUser;
+import rinde.sim.core.model.RoadModel;
+import rinde.sim.core.model.communication.CommunicationAPI;
+import rinde.sim.core.model.communication.CommunicationUser;
+import rinde.sim.core.model.communication.Mailbox;
+import rinde.sim.core.model.communication.Message;
+import rinde.sim.lab.common.ConfirmationMessage;
+import rinde.sim.lab.common.TransportRequest;
+
+public class TruckAgent implements TickListener, MovingRoadUser, SimulatorUser, CommunicationUser {
+	protected static final Logger LOGGER = LoggerFactory.getLogger(Simulator.class); 
+
+	private double speed;
+	private Point startingLocation;
+	private RoadModel rm;
+	private Queue<Point> path;
+	private RandomGenerator rand;
+	//reference to the communication API
+	private CommunicationAPI cm;
+	private Mailbox mailbox;
+	private String name;
+	private long currentFlag;
+	private CommunicationUser depot;
+	
+	public TruckAgent(String name, double speed, Point startingLocation){
+		this.speed = speed;
+		this.startingLocation = startingLocation;
+		this.name = name;
+		this.mailbox = new Mailbox();
+	}
+	
+	@Override
+	public void initRoadUser(RoadModel model) {
+		this.rm = model;
+		this.rm.addObjectAt(this, startingLocation);
+	}
+
+	@Override
+	public void setSimulator(SimulatorAPI api) {
+		this.rand = api.getRandomGenerator();
+	}
+
+	@Override
+	public double getSpeed() {
+		return this.speed;
+	}
+
+	@Override
+	public void tick(long currentTime, long timeStep) {
+		Queue<Message> messages = mailbox.getMessages();
+		for(Message message: messages){
+			if(message instanceof TransportRequest){
+				TransportRequest request = (TransportRequest) message;
+				Point flagLocation = request.getLocation();
+				depot = request.getSender();
+				currentFlag = request.getFlagID();
+				this.path = new LinkedList<Point>(rm.getShortestPathTo(this, flagLocation));
+			}
+		}
+		
+		if(path != null){
+			if(path.isEmpty()){
+				cm.send(depot,new ConfirmationMessage(this, name, currentFlag));
+			}else{
+				rm.followPath(this, path, timeStep);
+			}
+		}
+	}
+
+	@Override
+	public void afterTick(long currentTime, long timeStep) {
+		//not used
+	}
+
+	@Override
+	public void setCommunicationAPI(CommunicationAPI api) {
+		this.cm = api;
+	}
+
+	@Override
+	public Point getPosition() {
+		return this.rm.getPosition(this);
+	}
+
+	@Override
+	public double getRadius() {
+		//distances are ignored in the communication model
+		return 0;
+	}
+
+	@Override
+	public double getReliability() {
+		//we do not consider reliability in this example
+		return 1;
+	}
+
+	@Override
+	public void receive(Message message) {
+		this.mailbox.receive(message);
+	}
+
+}
