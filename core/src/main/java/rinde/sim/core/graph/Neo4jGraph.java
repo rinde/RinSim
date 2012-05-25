cbd22dfac7d8dc45bb1a8aefca88ae81a65ebb2a
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-05-25 12:59:37 +0200

    experimentation with neo4j integration into RinSim, not successful yet..

--- core/src/main/java/rinde/sim/core/graph/Neo4jGraph.java
+++ core/src/main/java/rinde/sim/core/graph/Neo4jGraph.java
@@ -4,14 +4,25 @@
 package rinde.sim.core.graph;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.math.random.RandomGenerator;
+import org.neo4j.collections.rtree.Listener;
+import org.neo4j.gis.spatial.Layer;
+import org.neo4j.gis.spatial.SimplePointLayer;
+import org.neo4j.gis.spatial.SpatialDatabaseRecord;
 import org.neo4j.gis.spatial.SpatialDatabaseService;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.tooling.GlobalGraphOperations;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 
 /**
  * @author Rinde van Lon (rinde.vanlon@cs.kuleuven.be)
@@ -19,13 +30,45 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
  */
 public class Neo4jGraph<E extends EdgeData> implements Graph<E> {
 
-	public Neo4jGraph() {
-		final GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("testdb");
-		SpatialDatabaseService db = new SpatialDatabaseService(graphDb);
+	private final static String LAYER_NAME = "test-layer";
+
+	private final GraphDatabaseService graphDb;
+	private final SpatialDatabaseService spatialService;
+	private final SimplePointLayer layer;
+	private final GlobalGraphOperations global;
+
+	public Neo4jGraph(String databaseName, boolean createNew) {
+		graphDb = new EmbeddedGraphDatabase(databaseName);
+		spatialService = new SpatialDatabaseService(graphDb);
+		if (createNew) {
+			clear();
+		}
+		// layer = spatialService.getOrCreatePointLayer("test-layer", null,
+		// null);
+
+		global = GlobalGraphOperations.at(graphDb);
+
+		Layer l = spatialService.getLayer(LAYER_NAME);
+		if (l == null) {
+			layer = spatialService.createSimplePointLayer(LAYER_NAME);
+		} else {
+			layer = (SimplePointLayer) l;
+		}
+
+		// layer = spatialService.createSimplePointLayer("test-layer");
+
+		// Coordinate
+		// Node n = graphDb.createNode();
+		// n.
+
+		// layer.fin
+
+		// layer.add(geomNode)
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
+				System.out.println("shutting graphdb down..");
 				graphDb.shutdown();
 			}
 		});
@@ -49,6 +92,21 @@ public class Neo4jGraph<E extends EdgeData> implements Graph<E> {
 	 */
 	@Override
 	public Collection<Point> getOutgoingConnections(Point node) {
+
+		point2coordinate(node);
+
+		// layer.add(coordinate)
+
+		// layer.getSpatialDatabase()
+
+		// layer.getIndex().count()
+
+		// layer.getIndex().
+
+		// graphDb.get
+
+		// layer.getSpatialDatabase().
+
 		throw new NotImplementedException();
 	}
 
@@ -138,7 +196,11 @@ public class Neo4jGraph<E extends EdgeData> implements Graph<E> {
 	 */
 	@Override
 	public Set<Point> getNodes() {
-		throw new NotImplementedException();
+		LinkedHashSet<Point> set = new LinkedHashSet<Point>();
+		for (Geometry g : layer.getDataset().getAllGeometries()) {
+			set.add(coordinate2point(g.getCoordinate()));
+		}
+		return set;
 	}
 
 	/*
@@ -159,9 +221,37 @@ public class Neo4jGraph<E extends EdgeData> implements Graph<E> {
 	 */
 	@Override
 	public void addConnection(Point from, Point to) {
-		throw new NotImplementedException();
+		Transaction tx = graphDb.beginTx();
+		try {
+			SpatialDatabaseRecord sdrFrom = layer.add(point2coordinate(from));
+
+			// layer.getIndex().
+
+			// layer.add(geomNode)
+			// GeometryFactory.toLineStringArray(lineStrings)
+
+			System.out.println(sdrFrom.getId());
+			SpatialDatabaseRecord sdrTo = layer.add(point2coordinate(to));
+			System.out.println(sdrTo.getId());
+			sdrFrom.getGeomNode().createRelationshipTo(sdrTo.getGeomNode(), GraphRelationshipTypes.CONNECTION);
+			tx.success();
+		} finally {
+			tx.finish();
+		}
 
 	}
+
+	Coordinate point2coordinate(Point p) {
+		return new Coordinate(p.x, p.y);
+	}
+
+	Point coordinate2point(Coordinate c) {
+		return new Point(c.x, c.y);
+	}
+
+	// Point node2point(Node n) {
+	// return n.g
+	// }
 
 	/*
 	 * (non-Javadoc)
@@ -252,6 +342,25 @@ public class Neo4jGraph<E extends EdgeData> implements Graph<E> {
 	@Override
 	public Point getRandomNode(RandomGenerator generator) {
 		throw new NotImplementedException();
+	}
+
+	public void clear() {
+		if (spatialService.getLayer(LAYER_NAME) != null) {
+			spatialService.deleteLayer(LAYER_NAME, new Listener() {
+				@Override
+				public void begin(int unitsOfWork) {}
+
+				@Override
+				public void worked(int workedSinceLastNotification) {}
+
+				@Override
+				public void done() {}
+			});
+		}
+	}
+
+	enum GraphRelationshipTypes implements RelationshipType {
+		CONNECTION
 	}
 
 }
--- core/src/test/java/rinde/sim/core/graph/Neo4jGraphTest.java
+++ core/src/test/java/rinde/sim/core/graph/Neo4jGraphTest.java
@@ -13,7 +13,15 @@ public class Neo4jGraphTest {
 
 	@Test
 	public void testSetup() {
-		Graph<LengthEdgeData> g = new Neo4jGraph<LengthEdgeData>();
+		Graph<LengthEdgeData> g = new Neo4jGraph<LengthEdgeData>("testdb", true);
+		System.out.println("before: " + g.getNodes());
+
+		g.addConnection(new Point(0, 0), new Point(10, 0));
+		g.addConnection(new Point(10, 0), new Point(20, 0));
+		g.addConnection(new Point(0, 0), new Point(20, 0));
+
+		System.out.println("after: " + g.getNodes());
+
 	}
 
 }
