e41b0b1b4d88cd4f11a501a9147bde3ea2b5b82b
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-07-27 17:35:46 +0200

    added default pdp scenario controller and events

---
+++ core/src/main/java/rinde/sim/core/model/pdp/PDPScenarioEvent.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package rinde.sim.core.model.pdp;
+
+/**
+ * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
+ * 
+ */
+public enum PDPScenarioEvent {
+
+    ADD_VEHICLE, REMOVE_VEHICLE, ADD_PARCEL, REMOVE_PARCEL, ADD_DEPOT, REMOVE_DEPOT;
+
+}
