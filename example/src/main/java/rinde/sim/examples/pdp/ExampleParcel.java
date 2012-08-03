0dd403fcd51a6f9fb22881c02ed1b513eba88c41
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-08-03 15:19:49 +0200

    fixed example

--- example/src/main/java/rinde/sim/examples/pdp/ExampleParcel.java
+++ example/src/main/java/rinde/sim/examples/pdp/ExampleParcel.java
@@ -7,6 +7,7 @@ import rinde.sim.core.graph.Point;
 import rinde.sim.core.model.pdp.PDPModel;
 import rinde.sim.core.model.pdp.Parcel;
 import rinde.sim.core.model.road.RoadModel;
+import rinde.sim.util.TimeWindow;
 
 /**
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
@@ -16,7 +17,7 @@ public class ExampleParcel extends Parcel {
 
 	public ExampleParcel(Point startPosition, Point pDestination, int pLoadingDuration, int pUnloadingDuration,
 			double magnitude) {
-		super(pDestination, pLoadingDuration, pUnloadingDuration, magnitude);
+		super(pDestination, pLoadingDuration, TimeWindow.ALWAYS, pUnloadingDuration, TimeWindow.ALWAYS, magnitude);
 		setStartPosition(startPosition);
 	}
 
