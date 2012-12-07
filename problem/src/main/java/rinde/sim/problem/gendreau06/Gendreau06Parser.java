acfbe7e6431faf88ea3e30310efd504237d1dd23
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-12-07 15:25:09 +0100

    fixed typo in parser

--- problem/src/main/java/rinde/sim/problem/gendreau06/Gendreau06Parser.java
+++ problem/src/main/java/rinde/sim/problem/gendreau06/Gendreau06Parser.java
@@ -65,7 +65,7 @@ public final class Gendreau06Parser {
 		checkArgument(numVehicles > 0, "at least one vehicle is necessary in the scenario");
 		final ScenarioBuilder sb = new ScenarioBuilder(ADD_PARCEL, ADD_DEPOT, ADD_VEHICLE, TIME_OUT);
 
-		final String regex = "req_rapide_[1-5]_(540|240)_(24|33)";
+		final String regex = ".*req_rapide_[1-5]_(450|240)_(24|33)";
 		final Matcher m = Pattern.compile(regex).matcher(fileName);
 		checkArgument(m.matches(), "The filename must conform to the following regex: " + regex + " input was: "
 				+ fileName);
