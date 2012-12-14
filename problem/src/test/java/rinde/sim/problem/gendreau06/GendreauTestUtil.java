9397c55203d8249c8d1f88a38ae8ec47c3f97c1c
rinde <Rinde.vanLon@cs.kuleuven.be>
2012-12-14 11:34:35 +0100

    created test util

---
+++ problem/src/test/java/rinde/sim/problem/gendreau06/GendreauTestUtil.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package rinde.sim.problem.gendreau06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import rinde.sim.problem.common.AddParcelEvent;
+import rinde.sim.scenario.ScenarioBuilder;
+import rinde.sim.scenario.ScenarioBuilder.ScenarioCreator;
+import rinde.sim.scenario.TimedEvent;
+
+/**
+ * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
+ * 
+ */
+public class GendreauTestUtil {
+
+	public static Gendreau06Scenario create(Collection<? extends TimedEvent> pEvents, Set<Enum<?>> pSupportedTypes,
+			long ts) {
+		return new Gendreau06Scenario(pEvents, pSupportedTypes, ts);
+	}
+
+	public static Gendreau06Scenario create(Collection<AddParcelEvent> parcels) {
+		Gendreau06Scenario gs;
+		try {
+			gs = Gendreau06Parser.parse(new BufferedReader(new StringReader("")), "req_rapide_1_240_24", 1, 1000L);
+		} catch (final IOException e) {
+			throw new RuntimeException("if this is thrown it is due to a programming error in the line above");
+		}
+		final ScenarioBuilder sb = new ScenarioBuilder(gs.getPossibleEventTypes());
+		sb.addEvents(gs.asList());
+		return sb.build(new ScenarioCreator<Gendreau06Scenario>() {
+			@Override
+			public Gendreau06Scenario create(List<TimedEvent> eventList, Set<Enum<?>> eventTypes) {
+				return GendreauTestUtil.create(eventList, eventTypes, 1000);
+			}
+		});
+	}
+
+}
