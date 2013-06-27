c6583077d484cd4c653a802dff810c7fb1c6cb16
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-06-27 14:17:32 +0200

    moved some test from the simulation test

--- core/src/test/java/rinde/sim/core/model/time/TimeModelTest.java
+++ core/src/test/java/rinde/sim/core/model/time/TimeModelTest.java
@@ -20,4 +20,26 @@ public class TimeModelTest {
 
     }
 
+    @Test
+    public void testTicks() {
+        assertEquals(0L, simulator.getCurrentTime());
+        final TickListenerImpl tl = new TickListenerImpl();
+        assertEquals(0, tl.getTickCount());
+        simulator.addTickListener(tl);
+        simulator.tick();
+        assertEquals(100L, simulator.getCurrentTime());
+        assertEquals(1, tl.getTickCount());
+        simulator.removeTickListener(tl);
+        simulator.tick();
+        assertEquals(1, tl.getTickCount());
+    }
+
+    @Test
+    public void testTickOrder() {
+        assertEquals(100L, simulator.getTimeStep());
+        final TickListenerImpl normal = new TickListenerImpl();
+        simulator.addTickListener(normal);
+        simulator.tick();
+        assertTrue(normal.getExecTime() < normal.getAfterExecTime());
+    }
 }
