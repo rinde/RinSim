1892494f42f5aeb752211d88b187b1108b2043c4
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-09 15:55:31 +0200

    started working on adding unit conversion using JScience library

--- central/ConverterTest.java
+++ central/ConverterTest.java
@@ -4,14 +4,24 @@
 package rinde.sim.central;
 
 import static java.util.Arrays.asList;
+import static javax.measure.unit.NonSI.KILOMETERS_PER_HOUR;
+import static javax.measure.unit.NonSI.MINUTE;
+import static javax.measure.unit.SI.KILOMETER;
+import static javax.measure.unit.SI.METER;
+import static javax.measure.unit.SI.MILLI;
+import static javax.measure.unit.SI.SECOND;
 import static org.junit.Assert.assertArrayEquals;
-import static rinde.sim.central.Converter.toTravelTimeMatrix;
 
 import java.math.RoundingMode;
+
+import javax.measure.Measure;
+import javax.measure.quantity.Velocity;
+import javax.measure.unit.ProductUnit;
 
 import org.junit.Test;
 
 import rinde.sim.core.graph.Point;
+import rinde.solver.pdptw.ArraysSolvers;
 
 /**
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
@@ -27,22 +37,27 @@ public class ConverterTest {
         final Point p3 = new Point(0, 10);
 
         // input in kilometers, output in minutes (rounded up), speed 40 km/h
-        final double factor1 = 60 / 40d;
-        final int[][] matrix1 = toTravelTimeMatrix(asList(p0, p1, p2, p3), factor1, RoundingMode.CEILING);
+        final Measure<Double, Velocity> speed1 = Measure
+                .valueOf(40d, KILOMETERS_PER_HOUR);
+        final int[][] matrix1 = ArraysSolvers
+                .toTravelTimeMatrix(asList(p0, p1, p2, p3), KILOMETER, speed1, MINUTE, RoundingMode.CEILING);
         assertArrayEquals(new int[] { 0, 15, 22, 15 }, matrix1[0]);
         assertArrayEquals(new int[] { 15, 0, 15, 22 }, matrix1[1]);
         assertArrayEquals(new int[] { 22, 15, 0, 15 }, matrix1[2]);
         assertArrayEquals(new int[] { 15, 22, 15, 0 }, matrix1[3]);
 
         final Point p4 = new Point(11, 3);
-        // input in meters, output in milliseconds (round down), speed 14.3 m/ms
-        final double factor2 = 14.3;
-        final int[][] matrix2 = toTravelTimeMatrix(asList(p0, p1, p2, p3, p4), factor2, RoundingMode.FLOOR);
+        // input in meters, output in milliseconds (round down), speed .0699
+        // m/ms
+        final Measure<Double, Velocity> speed2 = Measure
+                .valueOf(.0699, new ProductUnit<Velocity>(METER
+                        .divide(MILLI(SECOND))));
+        final int[][] matrix2 = ArraysSolvers
+                .toTravelTimeMatrix(asList(p0, p1, p2, p3, p4), METER, speed2, MILLI(SECOND), RoundingMode.FLOOR);
         assertArrayEquals(new int[] { 0, 143, 202, 143, 163 }, matrix2[0]);
         assertArrayEquals(new int[] { 143, 0, 143, 202, 45 }, matrix2[1]);
         assertArrayEquals(new int[] { 202, 143, 0, 143, 101 }, matrix2[2]);
         assertArrayEquals(new int[] { 143, 202, 143, 0, 186 }, matrix2[3]);
         assertArrayEquals(new int[] { 163, 45, 101, 186, 0 }, matrix2[4]);
     }
-
 }
