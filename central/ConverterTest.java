af8f0ffea675475ceaee16969a935a82da534611
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-13 16:44:28 +0200

    Solver is now validated as well, updated docs in pdptw package

--- central/ConverterTest.java
+++ central/ConverterTest.java
@@ -3,6 +3,18 @@
  */
 package rinde.sim.central;
 
+import javax.measure.quantity.Duration;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Velocity;
+import javax.measure.unit.Unit;
+
+import rinde.sim.central.GlobalStateObject.VehicleState;
+import rinde.sim.core.graph.Point;
+import rinde.sim.problem.common.ParcelDTO;
+import rinde.sim.problem.common.VehicleDTO;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 /**
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
@@ -10,4 +22,18 @@ package rinde.sim.central;
  */
 public class ConverterTest {
 
+    public static GlobalStateObject createGlobalStateObject(
+            ImmutableSet<ParcelDTO> availableParcels,
+            ImmutableList<VehicleState> vehicles, long time,
+            Unit<Duration> timeUnit, Unit<Velocity> speedUnit,
+            Unit<Length> distUnit) {
+        return new GlobalStateObject(availableParcels, vehicles, time,
+                timeUnit, speedUnit, distUnit);
+    }
+
+    public static VehicleState createVehicleState(VehicleDTO dto,
+            Point location, ImmutableSet<ParcelDTO> contents,
+            long remainingServiceTime) {
+        return new VehicleState(dto, location, contents, remainingServiceTime);
+    }
 }
