438528e7071dcd50790d9e946e770557c8cd8d47
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-14 14:37:52 +0200

    created special roadmodel which does not allow diversion

---
+++ problem/src/main/java/rinde/sim/problem/common/NoDiversionPlaneRoadModel.java
@@ -0,0 +1,207 @@
+package rinde.sim.problem.common;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.Maps.newLinkedHashMap;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import rinde.sim.core.TimeLapse;
+import rinde.sim.core.graph.Point;
+import rinde.sim.core.model.road.MoveEvent;
+import rinde.sim.core.model.road.MoveProgress;
+import rinde.sim.core.model.road.MovingRoadUser;
+import rinde.sim.core.model.road.PlaneRoadModel;
+import rinde.sim.core.model.road.RoadUser;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+
+/**
+ * A {@link PlaneRoadModel} which does not allow diversion of vehicles.
+ * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
+ */
+public class NoDiversionPlaneRoadModel extends PlaneRoadModel {
+
+    final Map<MovingRoadUser, DestinationObject> destinations;
+    final Multimap<MovingRoadUser, DestinationObject> destinationHistory;
+
+    /**
+     * Create a new plane road model using the specified boundaries and max
+     * speed.
+     * @param pMin The minimum x and y of the plane.
+     * @param pMax The maximum x and y of the plane.
+     * @param useSpeedConversion Use speed conversion.
+     * @param pMaxSpeed The maximum speed that objects can travel on the plane.
+     */
+    public NoDiversionPlaneRoadModel(Point pMin, Point pMax,
+            boolean useSpeedConversion, double pMaxSpeed) {
+        super(pMin, pMax, useSpeedConversion, pMaxSpeed);
+        destinations = newLinkedHashMap();
+        // does not allow duplicates: WE NEED THIS
+        destinationHistory = LinkedHashMultimap.create();
+    }
+
+    private void checkType(RoadUser ru) {
+        checkArgument(ru instanceof DefaultVehicle
+                || ru instanceof DefaultDepot || ru instanceof DefaultParcel, "This RoadModel only allows instances of DefaultVehicle, DefaultDepot and DefaultParcel.");
+    }
+
+    @Override
+    public void addObjectAt(RoadUser newObj, Point pos) {
+        checkType(newObj);
+        super.addObjectAt(newObj, pos);
+    }
+
+    @Override
+    public void addObjectAtSamePosition(RoadUser newObj, RoadUser existingObj) {
+        checkType(newObj);
+        super.addObjectAtSamePosition(newObj, existingObj);
+    }
+
+    @Override
+    public MoveProgress moveTo(MovingRoadUser object,
+            RoadUser destinationObject, TimeLapse time) {
+
+        DestinationObject newDestinationObject;
+        if (destinationObject instanceof DefaultParcel) {
+            final DefaultParcel dp = (DefaultParcel) destinationObject;
+            final DestType type = containsObject(dp) ? DestType.PICKUP
+                    : DestType.DELIVERY;
+            final Point pos = type == DestType.PICKUP ? dp.dto.pickupLocation
+                    : dp.dto.destinationLocation;
+            newDestinationObject = new DestinationObject(type, pos, dp);
+        } else {
+            newDestinationObject = new DestinationObject(DestType.DEPOT,
+                    getPosition(destinationObject), destinationObject);
+        }
+
+        boolean destChange = true;
+        if (destinations.containsKey(object)) {
+            final DestinationObject prev = destinations.get(object);
+            final boolean atDestination = getPosition(object).equals(prev.dest);
+            final boolean isOnMap = containsObject(prev.roadUser);
+
+            if (!atDestination && prev.type != DestType.DEPOT) {
+                // when we haven't reached our destination and the destination
+                // isn't the depot we are not allowed to change destination
+                checkArgument(prev.roadUser == destinationObject, "Diversion from the current destination is not allowed: %s.", prev.dest);
+                destChange = false;
+            } else {
+                // change destination
+                if (prev.type == DestType.PICKUP) {
+                    // when we are at the prev destination, and it was a pickup,
+                    // we are allowed to move if it has been picked up
+                    checkArgument(!isOnMap, "Can not move away before the parcel has been picked up: %s.", prev.roadUser);
+                } else if (prev.type == DestType.DELIVERY) {
+                    // when we are at the prev destination and it was a
+                    // delivery, we are allowed to move to other objects only
+                    checkArgument(prev.roadUser != destinationObject, "Can not move to the same parcel again, it has already been picked up: %s.", prev.roadUser);
+                } else {// it is a depot
+                    // the destination is only changed in case we are no longer
+                    // going towards the depot
+                    destChange = newDestinationObject.type != DestType.DEPOT;
+                }
+            }
+        }
+        destinations.put(object, newDestinationObject);
+        if (destChange) {
+            checkArgument(!destinationHistory.containsEntry(object, newDestinationObject), "It is not allowed to revisit this parcel, it should have been picked up and been delivered already: %s.", newDestinationObject.roadUser);
+            destinationHistory.put(object, newDestinationObject);
+        }
+        return move(object, newDestinationObject.dest, time);
+    }
+
+    private MoveProgress move(MovingRoadUser object, Point dest, TimeLapse time) {
+        // actual moving
+        Queue<Point> path;
+        if (objDestinations.containsKey(object)
+                && objDestinations.get(object).destination.equals(dest)) {
+            // is valid move? -> assume it is
+            path = objDestinations.get(object).path;
+        } else {
+            path = new LinkedList<Point>(getShortestPathTo(object, dest));
+            objDestinations.put(object, new DestinationPath(dest, path));
+        }
+        final MoveProgress mp = super.doFollowPath(object, path, time);
+        eventDispatcher.dispatchEvent(new MoveEvent(this, object, mp));
+        return mp;
+    }
+
+    /**
+     * Returns the destination of the specified {@link MovingRoadUser} if both:
+     * <ul>
+     * <li>The destination is a {@link DefaultParcel}.</li>
+     * <li>The {@link MovingRoadUser} has not yet reached its destination.</li>
+     * </ul>
+     * Returns <code>null</code> otherwise.
+     * @param obj The {@link MovingRoadUser} to check its destination for.
+     * @return The parcel the road user is heading to or <code>null</code>
+     *         otherwise.
+     */
+    @Nullable
+    public DefaultParcel getDestinationToParcel(MovingRoadUser obj) {
+        if (destinations.containsKey(obj)
+                && destinations.get(obj).type != DestType.DEPOT
+                && !destinations.get(obj).dest.equals(getPosition(obj))) {
+            return (DefaultParcel) destinations.get(obj).roadUser;
+        }
+        return null;
+    }
+
+    @Override
+    public MoveProgress moveTo(MovingRoadUser object, Point destination,
+            TimeLapse time) {
+        throw new UnsupportedOperationException(
+                "This road model only supports the moveTo(MovingRoadUser,RoadUser,TimeLapse) method.");
+    }
+
+    @Override
+    protected final MoveProgress doFollowPath(MovingRoadUser object,
+            Queue<Point> path, TimeLapse time) {
+        throw new UnsupportedOperationException(
+                "This road model only supports the moveTo(MovingRoadUser,RoadUser,TimeLapse) method.");
+    }
+
+    private static final class DestinationObject {
+        public final DestType type;
+        public final Point dest;
+        public final RoadUser roadUser;
+        private final int hashCode;
+
+        DestinationObject(DestType type, Point dest, RoadUser obj) {
+            this.type = type;
+            this.dest = dest;
+            roadUser = obj;
+            hashCode = new HashCodeBuilder(17, 31).append(type).append(dest)
+                    .append(obj).toHashCode();
+        }
+
+        @Override
+        public int hashCode() {
+            return hashCode;
+        }
+
+        // since this class is only used internally, we don't have to check for
+        // null or instanceof
+        @SuppressWarnings("null")
+        @Override
+        public boolean equals(@Nullable Object obj) {
+            final DestinationObject other = (DestinationObject) obj;
+            return new EqualsBuilder()//
+                    .append(type, other.type)//
+                    .append(dest, other.dest)//
+                    .append(roadUser, other.roadUser).isEquals();
+        }
+    }
+
+    enum DestType {
+        PICKUP, DELIVERY, DEPOT;
+    }
+}
---
+++ problem/src/test/java/rinde/sim/problem/common/NoDiversionPlaneRoadModelTest.java
@@ -0,0 +1,212 @@
+/**
+ * 
+ */
+package rinde.sim.problem.common;
+
+import static com.google.common.collect.Lists.newLinkedList;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import rinde.sim.core.TimeLapse;
+import rinde.sim.core.TimeLapseFactory;
+import rinde.sim.core.graph.Point;
+import rinde.sim.core.model.pdp.PDPModel;
+import rinde.sim.core.model.pdp.Parcel;
+import rinde.sim.core.model.road.RoadModel;
+import rinde.sim.util.TimeWindow;
+
+/**
+ * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
+ * 
+ */
+public class NoDiversionPlaneRoadModelTest {
+
+    DefaultParcel dp1;
+    DefaultParcel dp2;
+    DefaultParcel dp3;
+    DefaultDepot depot;
+
+    DefaultVehicle dv;
+
+    NoDiversionPlaneRoadModel rm;
+
+    static final TimeLapse TIME = TimeLapseFactory.create(0, 1);
+
+    static final TimeWindow DEFAULT_TW = new TimeWindow(0, 100);
+
+    @Before
+    public void setUp() {
+        rm = new NoDiversionPlaneRoadModel(new Point(0, 0), new Point(10, 10),
+                false, 0.1);
+        dp1 = create(new Point(1, 0), new Point(0, 7));
+        dp2 = create(new Point(5, 0), new Point(0, 5));
+        dp3 = create(new Point(1, 0), new Point(0, 6));
+        for (final DefaultParcel dp : asList(dp1, dp2, dp3)) {
+            rm.addObjectAt(dp, dp.dto.pickupLocation);
+        }
+
+        final Point depotLocation = new Point(5, 5);
+        depot = new DefaultDepot(depotLocation);
+        rm.addObjectAt(depot, depotLocation);
+
+        dv = new TestVehicle(new Point(0, 0));
+        rm.addObjectAt(dv, dv.getDTO().startPosition);
+
+        // to satisfy coverage tool
+        NoDiversionPlaneRoadModel.DestType.DEPOT.toString();
+        NoDiversionPlaneRoadModel.DestType.valueOf("DEPOT").toString();
+    }
+
+    @Test
+    public void test1() {
+        assertNull(rm.getDestinationToParcel(dv));
+        rm.moveTo(dv, dp1, time(7));
+        assertEquals(dp1, rm.getDestinationToParcel(dv));
+
+        rm.moveTo(dv, dp1, time(4));
+        assertNull(rm.getDestinationToParcel(dv));
+
+        rm.removeObject(dp1);
+        rm.moveTo(dv, dp1, time(1));
+        assertEquals(dp1, rm.getDestinationToParcel(dv));
+
+        rm.moveTo(dv, dp1, time(80));
+        assertNull(rm.getDestinationToParcel(dv));
+
+        rm.moveTo(dv, dp2, time(50));
+        assertEquals(dp2, rm.getDestinationToParcel(dv));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divertFromDest() {
+        rm.moveTo(dv, dp1, time(7));
+        rm.moveTo(dv, dp2, time(4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divertToSameBeforePickup() {
+        rm.moveTo(dv, dp1, time(7));
+        rm.moveTo(dv, dp1, time(4));
+        assertEquals(rm.getPosition(dv), rm.getPosition(dp1));
+        rm.moveTo(dv, dp1, time(80));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divertToOtherBeforePickup() {
+        rm.moveTo(dv, dp1, time(7));
+        rm.moveTo(dv, dp1, time(4));
+        assertEquals(rm.getPosition(dv), rm.getPosition(dp1));
+        rm.moveTo(dv, dp2, time(80));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divertToOtherBeforeDelivery() {
+        rm.moveTo(dv, dp1, time(10));
+        assertEquals(rm.getPosition(dv), rm.getPosition(dp1));
+        rm.removeObject(dp1);
+        rm.moveTo(dv, dp1, time(1));
+        rm.moveTo(dv, dp2, time(80));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void attemptVisitFor3rdTime() {
+        // first pickup
+        rm.moveTo(dv, dp1, time(11));
+        rm.removeObject(dp1);
+        // then deliver
+        rm.moveTo(dv, dp1, time(80));
+        // 3rd time is not allowed!
+        rm.moveTo(dv, dp1, time(80));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void attemptVisitFor3rdTime2() {
+        // first pickup
+        rm.moveTo(dv, dp1, time(11));
+        assertTrue(rm.equalPosition(dv, dp1));
+        rm.removeObject(dp1);
+        // then deliver
+        rm.moveTo(dv, dp1, time(80));
+        assertEquals(rm.getPosition(dv), dp1.getDestination());
+
+        rm.moveTo(dv, dp2, time(180));
+        assertTrue(rm.equalPosition(dv, dp2));
+        rm.removeObject(dp2);
+
+        // it is not allowed to revisit this position
+        rm.moveTo(dv, dp1, time(80));
+    }
+
+    @Test
+    public void moveToDepot() {
+        rm.moveTo(dv, depot, time(1));
+        assertNull(rm.getDestinationToParcel(dv));
+        rm.moveTo(dv, depot, time(1));
+        rm.moveTo(dv, dp1, time(1));
+        assertEquals(dp1, rm.getDestinationToParcel(dv));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidRoadUser() {
+        // does not exist
+        rm.moveTo(dv, new PlainTestParcel(new Point(6, 6)), time(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidRoadUser2() {
+        rm.addObjectAtSamePosition(create(new Point(1, 1), new Point(2, 2)), dp2);
+        rm.addObjectAtSamePosition(new PlainTestParcel(new Point(6, 6)), dp2);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    @SuppressWarnings("null")
+    public void invalidMoveTo() {
+        rm.moveTo(null, new Point(0, 0), null);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    @SuppressWarnings("null")
+    public void invalidFollowPath() {
+        rm.followPath(dv, newLinkedList(rm
+                .getShortestPathTo(new Point(0, 0), new Point(10, 10))), time(1));
+    }
+
+    static TimeLapse time(long t) {
+        return TimeLapseFactory.create(0, t);
+    }
+
+    static DefaultParcel create(Point p1, Point p2) {
+        return new DefaultParcel(new ParcelDTO(p1, p2, DEFAULT_TW, DEFAULT_TW,
+                0, 0, 0, 0));
+    }
+
+    static class TestVehicle extends DefaultVehicle {
+        public TestVehicle(Point start) {
+            super(new VehicleDTO(start, 10, 0, DEFAULT_TW));
+        }
+
+        @Override
+        protected void tickImpl(TimeLapse time) {}
+
+        @Override
+        public String toString() {
+            return "TestVehicle";
+        }
+    }
+
+    static class PlainTestParcel extends Parcel {
+        PlainTestParcel(Point pDestination) {
+            super(pDestination, 0, DEFAULT_TW, 0, DEFAULT_TW, 0);
+        }
+
+        @Override
+        public void initRoadPDP(RoadModel pRoadModel, PDPModel pPdpModel) {}
+    }
+
+}
