b6697e4f2321aca2cb1a9f2636f1c59a61150a8a
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-08-14 16:54:26 +0200

    added diversion checks

--- pdptw/central/ConverterTest.java
+++ pdptw/central/ConverterTest.java
@@ -3,19 +3,6 @@
  */
 package rinde.sim.pdptw.central;
 
-import javax.measure.quantity.Duration;
-import javax.measure.quantity.Length;
-import javax.measure.quantity.Velocity;
-import javax.measure.unit.Unit;
-
-import rinde.sim.core.graph.Point;
-import rinde.sim.pdptw.central.GlobalStateObject;
-import rinde.sim.pdptw.central.GlobalStateObject.VehicleState;
-import rinde.sim.problem.common.ParcelDTO;
-import rinde.sim.problem.common.VehicleDTO;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 
 /**
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
@@ -23,18 +10,4 @@ import com.google.common.collect.ImmutableSet;
  */
 public class ConverterTest {
 
-    public static GlobalStateObject createGlobalStateObject(
-            ImmutableSet<ParcelDTO> availableParcels,
-            ImmutableList<VehicleState> vehicles, long time,
-            Unit<Duration> timeUnit, Unit<Velocity> speedUnit,
-            Unit<Length> distUnit) {
-        return new GlobalStateObject(availableParcels, vehicles, time,
-                timeUnit, speedUnit, distUnit);
-    }
-
-    public static VehicleState createVehicleState(VehicleDTO dto,
-            Point location, ImmutableSet<ParcelDTO> contents,
-            long remainingServiceTime) {
-        return new VehicleState(dto, location, contents, remainingServiceTime);
-    }
 }
