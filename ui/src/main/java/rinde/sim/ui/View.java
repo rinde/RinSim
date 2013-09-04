d17030268f4ad91e711bcbe9022dcd571529bdbf
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-09-04 18:11:26 +0200

    removed sleak

--- ui/src/main/java/rinde/sim/ui/View.java
+++ ui/src/main/java/rinde/sim/ui/View.java
@@ -6,7 +6,6 @@ package rinde.sim.ui;
 import static com.google.common.base.Preconditions.checkState;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.DeviceData;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -14,7 +13,6 @@ import rinde.sim.core.Simulator;
 import rinde.sim.event.Event;
 import rinde.sim.event.Listener;
 import rinde.sim.ui.renderers.Renderer;
-import rinde.sim.ui.utils.Sleak;
 
 /**
  * @author Rinde van Lon (rinde.vanlon@cs.kuleuven.be)
@@ -23,86 +21,68 @@ import rinde.sim.ui.utils.Sleak;
  */
 public final class View {
 
-	/**
-	 * Indicates whether using the SWT handles tracing mode. Default value is
-	 * <code>false</code>.
-	 */
-	protected static boolean testingMode;
+  private static boolean autoPlay;
+  private static boolean autoClose;
 
-	/**
-	 * Indicates whether to start the simulation immediatly after launch.
-	 * Default value is <code>false</code>.
-	 */
-	protected static boolean autoPlay;
+  private View() {}
 
-	/**
-	 * Indicates whether to stop/close the app when the simulation has stopped.
-	 * Default value is <code>false</code>.
-	 */
-	protected static boolean autoClose;
+  /**
+   * Indicates whether to start the simulation immediatly after launch. Default
+   * value is <code>false</code>.
+   * @param pAutoPlay auto play.
+   */
+  public static void setAutoPlay(boolean pAutoPlay) {
+    View.autoPlay = pAutoPlay;
+  }
 
-	private View() {}
+  /**
+   * Indicates whether to stop/close the app when the simulation has stopped.
+   * Default value is <code>false</code>.
+   * @param pAutoClose auto close.
+   */
+  public static void setAutoClose(boolean pAutoClose) {
+    View.autoClose = pAutoClose;
+  }
 
-	/**
-	 * Define the SWT handles tracing mode. Disabled by default
-	 */
-	public static void setTestingMode(boolean pTestingMode) {
-		View.testingMode = pTestingMode;
-	}
+  @SuppressWarnings("unused")
+  public static void startGui(final Simulator simulator, final int speedup,
+      Renderer... renderers) {
+    checkState(
+        simulator.isConfigured(),
+        "Simulator needs to be configured before it can be visualized, see Simulator.configure()");
+    Display.setAppName("RinSim");
+    final Display display = new Display();
 
-	public static void setAutoPlay(boolean pAutoPlay) {
-		View.autoPlay = pAutoPlay;
-	}
+    final Shell shell = new Shell(display, SWT.TITLE | SWT.CLOSE | SWT.RESIZE);
+    shell.setText("RinSim - Simulator");
+    shell.setSize(new org.eclipse.swt.graphics.Point(800, 600));
 
-	public static void setAutoClose(boolean pAutoClose) {
-		View.autoClose = pAutoClose;
-	}
+    if (autoClose) {
+      simulator.getEventAPI().addListener(new Listener() {
+        @Override
+        public void handleEvent(final Event arg0) {
+          display.asyncExec(new Runnable() {
+            @Override
+            public void run() {
+              shell.close();
+            }
+          });
+        }
+      }, Simulator.SimulatorEventType.STOPPED);
+    }
 
-	@SuppressWarnings("unused")
-	public static void startGui(final Simulator simulator, final int speedup, Renderer... renderers) {
-		checkState(simulator.isConfigured(), "Simulator needs to be configured before it can be visualized, see Simulator.configure()");
-		Display.setAppName("RinSim");
-		final Display display;
-		if (testingMode) {
-			final DeviceData data = new DeviceData();
-			data.tracking = true;
-			display = new Display(data);
-			final Sleak sleak = new Sleak();
-			sleak.open();
-		} else {
-			display = new Display();
-		}
+    // simulator viewer is run in here
+    new SimulationViewer(shell, simulator, speedup, autoPlay, renderers);
 
-		final Shell shell = new Shell(display, SWT.TITLE | SWT.CLOSE | SWT.RESIZE);
-		shell.setText("RinSim - Simulator");
-		shell.setSize(new org.eclipse.swt.graphics.Point(800, 600));
+    shell.open();
 
-		if (autoClose) {
-			simulator.getEventAPI().addListener(new Listener() {
-				@Override
-				public void handleEvent(final Event arg0) {
-					display.asyncExec(new Runnable() {
-						@Override
-						public void run() {
-							shell.close();
-						}
-					});
-				}
-			}, Simulator.SimulatorEventType.STOPPED);
-		}
-
-		// simulator viewer is run in here
-		new SimulationViewer(shell, simulator, speedup, autoPlay, renderers);
-
-		shell.open();
-
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-		if (shell.isDisposed()) {
-			simulator.stop();
-		}
-	}
+    while (!shell.isDisposed()) {
+      if (!display.readAndDispatch()) {
+        display.sleep();
+      }
+    }
+    if (shell.isDisposed()) {
+      simulator.stop();
+    }
+  }
 }
