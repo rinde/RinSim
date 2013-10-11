f93ecf00531c2f07cbe3ced37d7adf3bdf960767
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-10-11 09:50:53 +0200

    improved doc

--- core/src/main/java/rinde/sim/core/model/road/MoveEvent.java
+++ core/src/main/java/rinde/sim/core/model/road/MoveEvent.java
@@ -9,18 +9,28 @@ import rinde.sim.core.model.road.AbstractRoadModel.RoadEventType;
 import rinde.sim.event.Event;
 
 /**
+ * Event representing a move of a {@link MovingRoadUser}.
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
- * 
  */
 public class MoveEvent extends Event {
-
-  public final RoadModel roadModel;
-  public final MovingRoadUser roadUser;
-  public final MoveProgress pathProgress;
+  private static final long serialVersionUID = 5118819834474109451L;
 
   /**
-     */
-  public MoveEvent(RoadModel rm, MovingRoadUser ru, MoveProgress pp) {
+   * The {@link RoadModel} that dispatched this event.
+   */
+  public final RoadModel roadModel;
+
+  /**
+   * The {@link MovingRoadUser} that moved.
+   */
+  public final MovingRoadUser roadUser;
+
+  /**
+   * Object containing the distance, time and path of this move.
+   */
+  public final MoveProgress pathProgress;
+
+  MoveEvent(RoadModel rm, MovingRoadUser ru, MoveProgress pp) {
     super(RoadEventType.MOVE, rm);
     roadModel = rm;
     roadUser = ru;
@@ -31,5 +41,4 @@ public class MoveEvent extends Event {
   public String toString() {
     return ToStringBuilder.reflectionToString(this);
   }
-
 }
