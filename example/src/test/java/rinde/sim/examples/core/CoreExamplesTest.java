a4444891befb0588fa683eb9d25ffe1a383ffbbd
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-02-17 10:38:24 +0100

    changed CoreExamplesTest

--- example/src/test/java/rinde/sim/examples/core/CoreExamplesTest.java
+++ example/src/test/java/rinde/sim/examples/core/CoreExamplesTest.java
@@ -1,6 +1,5 @@
 package rinde.sim.examples.core;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import rinde.sim.examples.core.comm.AgentCommunicationExample;
@@ -9,23 +8,24 @@ import rinde.sim.ui.UITestTools;
 
 public class CoreExamplesTest {
 
-  @Before
-  public void setUp() {
-    UITestTools.startAndClose(5000);
-  }
-
   @Test
   public void taxiExample() {
+    System.out.println("taxi example");
+    UITestTools.startAndClose(5000);
     TaxiExample.main(new String[] {});
   }
 
   @Test
   public void simpleExample() {
+    System.out.println("simple example");
+    UITestTools.startAndClose(5000);
     SimpleExample.main(null);
   }
 
   @Test
   public void communicationExample() throws Exception {
+    System.out.println("communication example");
+    UITestTools.startAndClose(5000);
     AgentCommunicationExample.main(new String[] {});
   }
 
