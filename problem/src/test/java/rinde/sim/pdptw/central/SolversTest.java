3d630c89a3c23fa21dfc088bbd88c6eebc9d4f3f
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-02-26 17:12:04 +0100

    added test for decomposition of global state object and consistency with Solvers.computeStats()

--- problem/src/test/java/rinde/sim/pdptw/central/SolversTest.java
+++ problem/src/test/java/rinde/sim/pdptw/central/SolversTest.java
@@ -51,10 +51,13 @@ import rinde.sim.pdptw.central.arrays.MultiVehicleSolverAdapter;
 import rinde.sim.pdptw.central.arrays.RandomMVArraysSolver;
 import rinde.sim.pdptw.common.DefaultParcel;
 import rinde.sim.pdptw.common.DefaultVehicle;
+import rinde.sim.pdptw.common.ObjectiveFunction;
 import rinde.sim.pdptw.common.PDPRoadModel;
 import rinde.sim.pdptw.common.PDPTWTestUtil;
 import rinde.sim.pdptw.common.ParcelDTO;
+import rinde.sim.pdptw.common.StatisticsDTO;
 import rinde.sim.pdptw.common.VehicleDTO;
+import rinde.sim.pdptw.gendreau06.Gendreau06ObjectiveFunction;
 import rinde.sim.util.TimeWindow;
 
 import com.google.common.collect.ImmutableList;
@@ -227,6 +230,92 @@ public class SolversTest {
         .noCurrentRoutes());
     assertTrue(sc6.state.vehicles.get(0).contents.isEmpty());
     assertTrue(sc6.state.availableParcels.isEmpty());
+  }
+
+  /**
+   * Tests whether the
+   * {@link Solvers#computeStats(GlobalStateObject, ImmutableList)} method
+   * produces the same result when providing decomposed state objects.
+   */
+  @Test
+  public void convertDecompositionTest() {
+    final VehicleDTO vd1 = new VehicleDTO(
+        new Point(5, 5), 30, 0,
+        new TimeWindow(100L, 100000L));
+
+    final ParcelDTO a = new ParcelDTO(
+        new Point(0, 0),
+        new Point(10, 10),
+        new TimeWindow(0, 30),
+        new TimeWindow(70, 80),
+        0, 0, 5000L, 10000L);
+
+    final ParcelDTO b = new ParcelDTO(
+        new Point(5, 0),
+        new Point(10, 7),
+        new TimeWindow(0, 30),
+        new TimeWindow(70, 80),
+        0, 0, 5000L, 10000L);
+
+    final ParcelDTO c = new ParcelDTO(
+        new Point(3, 0),
+        new Point(6, 7),
+        new TimeWindow(0, 30),
+        new TimeWindow(70, 80),
+        0, 0, 5000L, 10000L);
+
+    final ParcelDTO d = new ParcelDTO(
+        new Point(3, 0),
+        new Point(6, 2),
+        new TimeWindow(0, 30),
+        new TimeWindow(70, 80),
+        0, 0, 5000L, 10000L);
+
+    final ImmutableSet<ParcelDTO> availableParcels = ImmutableSet
+        .<ParcelDTO> builder()
+        .add(b).add(c)
+        .build();
+
+    final ImmutableList<VehicleStateObject> vehicles = ImmutableList
+        .<VehicleStateObject> builder()
+        .add(
+            new VehicleStateObject(
+                vd1,
+                new Point(7, 9),
+                ImmutableSet.<ParcelDTO> of(a),
+                0L,
+                null,
+                null))
+        .add(new VehicleStateObject(
+            vd1,
+            new Point(3, 2),
+            ImmutableSet.<ParcelDTO> of(d),
+            0L,
+            null,
+            null))
+        .build();
+
+    final GlobalStateObject state = new GlobalStateObject(availableParcels,
+        vehicles, 0L, SI.MILLI(SI.SECOND), NonSI.KILOMETERS_PER_HOUR,
+        SI.KILOMETER);
+
+    final ImmutableList<ImmutableList<ParcelDTO>> routes = ImmutableList
+        .<ImmutableList<ParcelDTO>> builder()
+        .add(ImmutableList.<ParcelDTO> of(a, b, c, c, b))
+        .add(ImmutableList.<ParcelDTO> of(d, d))
+        .build();
+
+    final StatisticsDTO stats = Solvers.computeStats(state, routes);
+    final ObjectiveFunction objFunc = new Gendreau06ObjectiveFunction();
+    final double cost = objFunc.computeCost(stats);
+
+    final double cost0 = objFunc.computeCost(Solvers.computeStats(
+        state.withSingleVehicle(0),
+        ImmutableList.of(routes.get(0))));
+    final double cost1 = objFunc.computeCost(Solvers.computeStats(
+        state.withSingleVehicle(1),
+        ImmutableList.of(routes.get(1))));
+    assertEquals(cost, cost0 + cost1, 0.001);
   }
 
   // doesn't check the contents!
