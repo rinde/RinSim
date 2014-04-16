27100faaa5264c7b339987de646aeeae0da42223
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-04-16 14:37:53 +0200

    moved locations to main generator package

--- problem/src/main/java/rinde/sim/pdptw/generator/Locations.java
+++ problem/src/main/java/rinde/sim/pdptw/generator/Locations.java
@@ -1,4 +1,4 @@
-package rinde.sim.pdptw.generator.loc;
+package rinde.sim.pdptw.generator;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static rinde.sim.util.SupplierRngs.uniformDouble;
@@ -17,6 +17,29 @@ public class Locations {
 
   public static Builder builder() {
     return new Builder();
+  }
+
+  /**
+   * A location generator generates locations for orders (aka tasks).
+   * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
+   */
+  public interface LocationGenerator {
+    /**
+     * Should generate locations for the specified number of orders (aka tasks).
+     * There should be enough locations for each order. Typically this is
+     * predefined as a ratio, e.g. <code>1:2</code> in case origin and
+     * destination is required for each order.
+     * @param seed The random seed.
+     * @param numOrders The number of orders for which a location is required.
+     * @return A list of locations for the orders.
+     */
+    ImmutableList<Point> generate(long seed, int numOrders);
+
+    Point getMin();
+
+    Point getMax();
+
+    Point getCenter();
   }
 
   public static class Builder {
