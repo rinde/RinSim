0670cd9ffbf02228573779a3ac3a983ff84d1fd8
Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
2014-04-25 11:59:17 +0200

    fixed failing test

--- core/src/test/java/rinde/sim/scenario/ScenarioBuilderTest.java
+++ core/src/test/java/rinde/sim/scenario/ScenarioBuilderTest.java
@@ -20,96 +20,103 @@ import rinde.sim.scenario.ScenarioBuilder.EventTypeFunction;
  */
 public class ScenarioBuilderTest {
 
-    protected ScenarioBuilder scenarioBuilder;
+  protected ScenarioBuilder scenarioBuilder;
 
-    enum TestEvents {
-        EVENT_A, EVENT_B;
-    }
+  enum TestEvents {
+    EVENT_A, EVENT_B;
+  }
 
-    @Before
-    public void setUp() {
-        scenarioBuilder = new ScenarioBuilder(EVENT_A, EVENT_B);
-    }
+  @Before
+  public void setUp() {
+    scenarioBuilder = new ScenarioBuilder(EVENT_A, EVENT_B);
+  }
 
-    @Test
-    public void addMultipleEvents() {
-        scenarioBuilder
-                .addEventGenerator(new ScenarioBuilder.MultipleEventGenerator<TimedEvent>(
-                        100, 3, new ScenarioBuilder.EventTypeFunction(EVENT_A)));
-        scenarioBuilder.addMultipleEvents(0, 2, EVENT_A);
-        final Scenario s = scenarioBuilder.build();
+  @Test
+  public void addMultipleEvents() {
+    scenarioBuilder
+        .addEventGenerator(new ScenarioBuilder.MultipleEventGenerator<TimedEvent>(
+            100, 3, new ScenarioBuilder.EventTypeFunction(EVENT_A)));
+    scenarioBuilder.addMultipleEvents(0, 2, EVENT_A);
+    final Scenario s = scenarioBuilder.build();
 
-        assertEquals(2, frequency(s.asList(), new TimedEvent(EVENT_A, 0)));
-        assertEquals(asList(new TimedEvent(EVENT_A, 0), new TimedEvent(EVENT_A,
-                0), new TimedEvent(EVENT_A, 100), new TimedEvent(EVENT_A, 100), new TimedEvent(
+    assertEquals(2, frequency(s.asList(), new TimedEvent(EVENT_A, 0)));
+    assertEquals(
+        asList(new TimedEvent(EVENT_A, 0), new TimedEvent(EVENT_A,
+            0), new TimedEvent(EVENT_A, 100), new TimedEvent(EVENT_A, 100),
+            new TimedEvent(
                 EVENT_A, 100)), s.asList());
-    }
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addMultipleEventsFail1() {
-        scenarioBuilder.addMultipleEvents(-1, 1, (EventCreator<?>) null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void addMultipleEventsFail1() {
+    scenarioBuilder.addMultipleEvents(-1, 1, (EventCreator<?>) null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addMultipleEventsFail2() {
-        scenarioBuilder.addMultipleEvents(0, 0, (EventCreator<?>) null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void addMultipleEventsFail2() {
+    scenarioBuilder.addMultipleEvents(0, 0, (EventCreator<?>) null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addMultipleEventsFail3() {
-        scenarioBuilder.addMultipleEvents(0, 1, (EventCreator<?>) null);
-    }
+  @Test(expected = NullPointerException.class)
+  public void addMultipleEventsFail3() {
+    scenarioBuilder.addMultipleEvents(0, 1, (EventCreator<?>) null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addNull() {
-        scenarioBuilder.addEventGenerator(null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void addNull() {
+    scenarioBuilder.addEventGenerator(null);
+  }
 
-    @Test
-    public void addTimeSeriesOfEvents() {
-        scenarioBuilder.addTimeSeriesOfEvents(0, 3, 1, EVENT_A);
-        assertEquals(asList(new TimedEvent(EVENT_A, 0), new TimedEvent(EVENT_A,
-                1), new TimedEvent(EVENT_A, 2), new TimedEvent(EVENT_A, 3)), scenarioBuilder
-                .build().asList());
+  @Test
+  public void addTimeSeriesOfEvents() {
+    scenarioBuilder.addTimeSeriesOfEvents(0, 3, 1, EVENT_A);
+    assertEquals(asList(new TimedEvent(EVENT_A, 0), new TimedEvent(EVENT_A,
+        1), new TimedEvent(EVENT_A, 2), new TimedEvent(EVENT_A, 3)),
+        scenarioBuilder
+            .build().asList());
 
-        scenarioBuilder.addTimeSeriesOfEvents(5, 10, 3, EVENT_A);
-        assertEquals(asList(new TimedEvent(EVENT_A, 0), new TimedEvent(EVENT_A,
-                1), new TimedEvent(EVENT_A, 2), new TimedEvent(EVENT_A, 3), new TimedEvent(
+    scenarioBuilder.addTimeSeriesOfEvents(5, 10, 3, EVENT_A);
+    assertEquals(
+        asList(new TimedEvent(EVENT_A, 0), new TimedEvent(EVENT_A,
+            1), new TimedEvent(EVENT_A, 2), new TimedEvent(EVENT_A, 3),
+            new TimedEvent(
                 EVENT_A, 5), new TimedEvent(EVENT_A, 8)), scenarioBuilder
-                .build().asList());
+            .build().asList());
 
-        scenarioBuilder.addEvent(new TimedEvent(EVENT_B, 0));
-        assertEquals(asList(new TimedEvent(EVENT_B, 0), new TimedEvent(EVENT_A,
-                0), new TimedEvent(EVENT_A, 1), new TimedEvent(EVENT_A, 2), new TimedEvent(
+    scenarioBuilder.addEvent(new TimedEvent(EVENT_B, 0));
+    assertEquals(
+        asList(new TimedEvent(EVENT_B, 0), new TimedEvent(EVENT_A,
+            0), new TimedEvent(EVENT_A, 1), new TimedEvent(EVENT_A, 2),
+            new TimedEvent(
                 EVENT_A, 3), new TimedEvent(EVENT_A, 5), new TimedEvent(
                 EVENT_A, 8)), scenarioBuilder.build().asList());
-    }
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addTimeSeriesOfEventsFail1() {
-        scenarioBuilder.addTimeSeriesOfEvents(0, 0, 0, (EventCreator<?>) null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void addTimeSeriesOfEventsFail1() {
+    scenarioBuilder.addTimeSeriesOfEvents(0, 0, 0, (EventCreator<?>) null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addTimeSeriesOfEventsFail2() {
-        scenarioBuilder
-                .addTimeSeriesOfEvents(-2, -1, 0, (EventCreator<?>) null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void addTimeSeriesOfEventsFail2() {
+    scenarioBuilder
+        .addTimeSeriesOfEvents(-2, -1, 0, (EventCreator<?>) null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addTimeSeriesOfEventsFail3() {
-        scenarioBuilder.addTimeSeriesOfEvents(0, 1, 0, (EventCreator<?>) null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void addTimeSeriesOfEventsFail3() {
+    scenarioBuilder.addTimeSeriesOfEvents(0, 1, 0, (EventCreator<?>) null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addTimeSeriesOfEventsFail4() {
-        scenarioBuilder.addTimeSeriesOfEvents(0, 1, 1, (EventCreator<?>) null);
-    }
+  @Test(expected = NullPointerException.class)
+  public void addTimeSeriesOfEventsFail4() {
+    scenarioBuilder.addTimeSeriesOfEvents(0, 1, 1, (EventCreator<?>) null);
+  }
 
-    @SuppressWarnings("unused")
-    @Test(expected = IllegalArgumentException.class)
-    public void eventTypeFunctionConstructor() {
-        new EventTypeFunction(null);
-    }
+  @SuppressWarnings("unused")
+  @Test(expected = IllegalArgumentException.class)
+  public void eventTypeFunctionConstructor() {
+    new EventTypeFunction(null);
+  }
 
 }
