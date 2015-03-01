cfa498fa1bd1aa87acc917a6b3d125ab5b416009
Rinde van Lon <rindevanlon@gmail.com>
2015-03-01 15:52:16 +0100

    created WarehouseExample

---
+++ example/src/main/java/com/github/rinde/rinsim/examples/warehouse/AGVAgent.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2011-2014 Rinde van Lon, iMinds DistriNet, KU Leuven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.rinde.rinsim.examples.warehouse;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.apache.commons.math3.random.RandomGenerator;
+
+import com.github.rinde.rinsim.core.TickListener;
+import com.github.rinde.rinsim.core.TimeLapse;
+import com.github.rinde.rinsim.core.model.road.CollisionGraphRoadModel;
+import com.github.rinde.rinsim.core.model.road.MovingRoadUser;
+import com.github.rinde.rinsim.core.model.road.RoadModel;
+import com.github.rinde.rinsim.geom.Point;
+import com.google.common.base.Optional;
+
+class AGVAgent implements TickListener, MovingRoadUser {
+
+  private final RandomGenerator rng;
+  private Optional<CollisionGraphRoadModel> roadModel;
+  private Optional<Point> destination;
+  private Queue<Point> path;
+
+  AGVAgent(RandomGenerator r) {
+    rng = r;
+    roadModel = Optional.absent();
+    destination = Optional.absent();
+    path = new LinkedList<>();
+  }
+
+  @Override
+  public void initRoadUser(RoadModel model) {
+    roadModel = Optional.of((CollisionGraphRoadModel) model);
+
+    Point p;
+    while (roadModel.get().isOccupied(p = model.getRandomPosition(rng))) {}
+    roadModel.get().addObjectAt(this, p);
+  }
+
+  @Override
+  public double getSpeed() {
+    return 1;
+  }
+
+  void nextDestination() {
+    destination = Optional.of(roadModel.get().getRandomPosition(rng));
+    path = new LinkedList<>(roadModel.get().getShortestPathTo(this,
+        destination.get()));
+  }
+
+  @Override
+  public void tick(TimeLapse timeLapse) {
+    if (!destination.isPresent()) {
+      nextDestination();
+    }
+
+    roadModel.get().followPath(this, path, timeLapse);
+
+    if (roadModel.get().getPosition(this).equals(destination.get())) {
+      nextDestination();
+    }
+  }
+
+  @Override
+  public void afterTick(TimeLapse timeLapse) {}
+
+}
---
+++ example/src/main/java/com/github/rinde/rinsim/examples/warehouse/WarehouseExample.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2011-2014 Rinde van Lon, iMinds DistriNet, KU Leuven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.rinde.rinsim.examples.warehouse;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.Map;
+
+import javax.measure.Measure;
+import javax.measure.unit.SI;
+
+import org.apache.commons.math3.random.MersenneTwister;
+import org.apache.commons.math3.random.RandomGenerator;
+
+import com.github.rinde.rinsim.core.Simulator;
+import com.github.rinde.rinsim.core.model.road.CollisionGraphRoadModel;
+import com.github.rinde.rinsim.geom.Graph;
+import com.github.rinde.rinsim.geom.Graphs;
+import com.github.rinde.rinsim.geom.LengthData;
+import com.github.rinde.rinsim.geom.ListenableGraph;
+import com.github.rinde.rinsim.geom.Point;
+import com.github.rinde.rinsim.geom.TableGraph;
+import com.github.rinde.rinsim.ui.View;
+import com.github.rinde.rinsim.ui.renderers.AGVRenderer;
+import com.github.rinde.rinsim.ui.renderers.WarehouseRenderer;
+import com.google.common.collect.ImmutableTable;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Table;
+
+/**
+ * Example showcasing the {@link CollisionGraphRoadModel} with an
+ * {@link WarehouseRenderer} and {@link AGVRenderer}.
+ * @author Rinde van Lon
+ */
+public class WarehouseExample {
+
+  private static final double VEHICLE_LENGTH = 2d;
+
+  /**
+   * @param args - No args.
+   */
+  public static void main(String[] args) {
+
+    final RandomGenerator rng = new MersenneTwister(123L);
+    final Simulator sim = new Simulator(rng, Measure.valueOf(1000L,
+        SI.MILLI(SI.SECOND)));
+
+    sim.register(CollisionGraphRoadModel.builder(createSimpleGraph()).build());
+    sim.configure();
+
+    for (int i = 0; i < 20; i++) {
+      sim.register(new AGVAgent(rng));
+    }
+
+    View.create(sim)
+        .with(WarehouseRenderer.builder()
+            .setMargin(VEHICLE_LENGTH)
+            .drawOneWayStreetArrows()
+        // .showNodeOccupancy()
+        )
+        .with(AGVRenderer.builder()
+            .useDifferentColorsForVehicles()
+            .showVehicleCreationNumber()
+        )
+        // .with(GraphRoadModelRenderer.builder()
+        // .showNodes()
+        // .showNodeLabels()
+        // )
+        .show();
+  }
+
+  static ImmutableTable<Integer, Integer, Point> createMatrix(int cols,
+      int rows, Point offset) {
+    final ImmutableTable.Builder<Integer, Integer, Point> builder = ImmutableTable
+        .builder();
+    for (int c = 0; c < cols; c++) {
+      for (int r = 0; r < rows; r++) {
+        builder.put(r, c, new Point(
+            offset.x + c * VEHICLE_LENGTH * 2,
+            offset.y + r * VEHICLE_LENGTH * 2));
+      }
+    }
+    return builder.build();
+  }
+
+  static ListenableGraph<LengthData> createSimpleGraph() {
+    final Graph<LengthData> g = new TableGraph<>();
+
+    final Table<Integer, Integer, Point> matrix = createMatrix(8, 6,
+        new Point(0, 0));
+
+    for (int i = 0; i < matrix.columnMap().size(); i++) {
+
+      Iterable<Point> path;
+      if (i % 2 == 0) {
+        path = Lists.reverse(newArrayList(matrix.column(i).values()));
+      } else {
+        path = matrix.column(i).values();
+      }
+      Graphs.addPath(g, path);
+    }
+
+    Graphs.addPath(g, matrix.row(0).values());
+    Graphs.addPath(g, Lists.reverse(newArrayList(matrix.row(
+        matrix.rowKeySet().size() - 1).values())));
+
+    return new ListenableGraph<>(g);
+  }
+
+  static ListenableGraph<LengthData> createGraph() {
+    final Graph<LengthData> g = new TableGraph<>();
+
+    final Table<Integer, Integer, Point> matrix = createMatrix(5, 10,
+        new Point(0, 0));
+    for (final Map<Integer, Point> column : matrix.columnMap().values()) {
+      Graphs.addBiPath(g, column.values());
+    }
+    Graphs.addBiPath(g, matrix.row(4).values());
+    Graphs.addBiPath(g, matrix.row(5).values());
+
+    final Table<Integer, Integer, Point> matrix2 = createMatrix(10, 7,
+        new Point(30, 6));
+    for (final Map<Integer, Point> row : matrix2.rowMap().values()) {
+      Graphs.addBiPath(g, row.values());
+    }
+    Graphs.addBiPath(g, matrix2.column(0).values());
+    Graphs.addBiPath(g, matrix2.column(matrix2.columnKeySet().size() -
+        1).values());
+
+    Graphs.addPath(g, matrix2.get(2, 0), matrix.get(4, 4));
+    Graphs.addPath(g, matrix.get(5, 4), matrix2.get(4, 0));
+
+    return new ListenableGraph<>(g);
+  }
+}
