c5ff7419c88e7b5549ccc96960cfff44528a8bf6
Rinde van Lon <rindevanlon@gmail.com>
2015-03-02 20:57:06 +0100

    updated license

--- experiment/src/main/java/com/github/rinde/rinsim/experiment/Experiment.java
+++ experiment/src/main/java/com/github/rinde/rinsim/experiment/Experiment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2014 Rinde van Lon, iMinds DistriNet, KU Leuven
+ * Copyright (C) 2011-2015 Rinde van Lon, iMinds-DistriNet, KU Leuven
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.rinde.rinsim.experiment;
 
 import static com.google.common.base.Preconditions.checkArgument;
--- scenario-util/src/main/java/com/github/rinde/rinsim/scenario/generator/Models.java
+++ scenario-util/src/main/java/com/github/rinde/rinsim/scenario/generator/Models.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2014 Rinde van Lon, iMinds DistriNet, KU Leuven
+ * Copyright (C) 2011-2015 Rinde van Lon, iMinds-DistriNet, KU Leuven
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
