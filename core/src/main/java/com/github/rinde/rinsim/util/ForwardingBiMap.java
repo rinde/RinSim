2c67010da2a8180e10518ee3975422a7ee936870
Rinde van Lon <rindevanlon@gmail.com>
2015-03-06 08:02:45 +0100

    created ForwardingBiMap

---
+++ core/src/main/java/com/github/rinde/rinsim/util/ForwardingBiMap.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2011-2014 Rinde van Lon, iMinds DistriNet, KU Leuven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.rinde.rinsim.util;
+
+import java.util.Set;
+
+import javax.annotation.Nullable;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ForwardingMap;
+
+/**
+ * @author Rinde van Lon
+ *
+ */
+public class ForwardingBiMap<K, V> extends ForwardingMap<K, V> implements
+    BiMap<K, V> {
+
+  private final BiMap<K, V> delegate;
+
+  protected ForwardingBiMap(BiMap<K, V> deleg) {
+    delegate = deleg;
+  }
+
+  @Override
+  public Set<V> values() {
+    return delegate().values();
+  }
+
+  @Override
+  public V forcePut(@Nullable K key, @Nullable V value) {
+    return delegate().forcePut(key, value);
+  }
+
+  @Override
+  public BiMap<V, K> inverse() {
+    return delegate().inverse();
+  }
+
+  @Override
+  protected BiMap<K, V> delegate() {
+    return delegate;
+  }
+}
