5ff4b647caa90f41fb8d0ead3b9c347ce12785ed
Rinde van Lon <rindevanlon@gmail.com>
2017-01-10 15:00:00 +0100

    updated license

--- core/src/test/java/com/github/rinde/rinsim/core/model/road/RoadTestUtil.java
+++ core/src/test/java/com/github/rinde/rinsim/core/model/road/RoadTestUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2016 Rinde van Lon, iMinds-DistriNet, KU Leuven
+ * Copyright (C) 2011-2017 Rinde van Lon, imec-DistriNet, KU Leuven
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
