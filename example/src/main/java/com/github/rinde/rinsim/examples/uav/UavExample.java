fb9ed093adc21edfd8065a7f7f72edf5c4560bb5
Hoang Tung Dinh <tunghdinh@gmail.com>
2017-04-14 18:16:59 +0200

    Fix some coding style issues

--- example/src/main/java/com/github/rinde/rinsim/examples/uav/UavExample.java
+++ example/src/main/java/com/github/rinde/rinsim/examples/uav/UavExample.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011-2017 Rinde van Lon, imec-DistriNet, KU Leuven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.rinde.rinsim.examples.uav;
 
 import com.github.rinde.rinsim.core.Simulator;
@@ -8,13 +23,17 @@ import com.github.rinde.rinsim.ui.renderers.PlaneRoadModelRenderer;
 
 import javax.measure.unit.SI;
 
-/** @author Hoang Tung Dinh */
+/**
+ * @author Hoang Tung Dinh
+ */
 public final class UavExample {
 
   static final int SPEED_UP = 8;
   static final Point MIN_POINT = new Point(0, 0);
   static final Point MAX_POINT = new Point(6000, 6000);
   static final double MAX_SPEED = 1000d;
+
+  private UavExample() {}
 
   public static void main(String[] args) {
 
