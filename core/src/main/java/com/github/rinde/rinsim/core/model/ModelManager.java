cc662dadac5e5837aa290229c7f78d1d4a7acde9
Rinde van Lon <rindevanlon@gmail.com>
2018-03-30 16:58:07 +0800

    updated license

--- core/src/main/java/com/github/rinde/rinsim/core/model/ModelManager.java
+++ core/src/main/java/com/github/rinde/rinsim/core/model/ModelManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2017 Rinde van Lon, imec-DistriNet, KU Leuven
+ * Copyright (C) 2011-2018 Rinde R.S. van Lon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
